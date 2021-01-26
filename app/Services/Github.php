1e1ed36986eab1871bf47b7f279798f80f6ac479
mohamed-foly <m.alaoo@yahoo.com>
2021-01-26 06:38:49 +0200

    implement required functions

--- app/Services/Github.php
+++ app/Services/Github.php
@@ -4,4 +4,16 @@ namespace App\Services;
 
 class Github {
 
+  private $programmingLang;
+
+  public function setProgrammingLanguage($programmingLang) {
+    $this->programmingLang = $programmingLang;
+    return $this;
+  }
+
+  public static function search($date, $order='desc', $per_page=10){
+    $result = null; // github response
+    return $result;
+  }
+
 }
